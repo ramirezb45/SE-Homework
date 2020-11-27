ed71f93ebe4516e08d53e1db2c0c5d55244d60f4
ramirezb45 <brittneyramirez79@icloud.com>
2020-11-27 17:35:40 -0500

    Add files via upload

---
+++ Week2_day1_morninglab.js
@@ -0,0 +1,38 @@
+ var testScore = 67;
+if (testScore > 90) {
+    console.log('A');
+}
+if (testScore > 80){
+    console.log('B');
+}
+if (testScore > 70){
+    console.log('C');
+}
+if (testScore > 55){
+    console.log('D');
+}
+if (testScore < 55){
+    console.log('F');
+}
+
+var time = 1500;
+if(time >= 0500 && time <= 1159){
+    console.log("Good Morning");
+} else if(time >= 1200 && time <= 1600){
+    console.log("Good Afternoon");
+} else{
+    console.log("Hey there");
+}
+
+var num = 40
+var num1 = 50
+if(num > num1 ){
+        console.log(num);
+}
+
+    else{ 
+        console.log(num1);
+
+}
+
+
